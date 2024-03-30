b5254f2afc323799c1425fad9d87de87906bed95
514K <alekseysafronov0@gmail.com>
2024-03-30 17:08:19 +0300

    add ultrasafe methods)))

--- main.py
+++ main.py
@@ -8,30 +8,36 @@ bot = AsyncTeleBot(token)
 
 @bot.message_handler(commands=['го', 'start'])
 async def send_welcome(message):
-    msgText = ""
-    pinMsg = False
-    if str(message.chat.id) in rooms.keys() and not isRoomComplete(rooms[str(message.chat.id)]):
-        msgText = "Вам необходимо закончить комнату - " + getRoomLink(rooms[str(message.chat.id)])
-    else:
-        roomId = getRoomId()
-        rooms[str(message.chat.id)] = roomId
-        msgText = "Ваша комната - " + getRoomLink(roomId)
-        pinMsg = True
-        
-    sm = await bot.send_message(message.chat.id, msgText)
-    if pinMsg:
-        await bot.pin_chat_message(message.chat.id, sm.message_id)
+    try:
+        msgText = ""
+        pinMsg = False
+        if str(message.chat.id) in rooms.keys() and not isRoomComplete(rooms[str(message.chat.id)]):
+            msgText = "Вам необходимо закончить комнату - " + getRoomLink(rooms[str(message.chat.id)])
+        else:
+            roomId = getRoomId()
+            rooms[str(message.chat.id)] = roomId
+            msgText = "Ваша комната - " + getRoomLink(roomId)
+            pinMsg = True
+            
+        sm = await bot.send_message(message.chat.id, msgText)
+        if pinMsg:
+            await bot.pin_chat_message(message.chat.id, sm.message_id)
+    except:
+        pass
 
 async def f():
     while True:
-        await asyncio.sleep(5)
-        for key in rooms:
-            if isRoomComplete(rooms[key]):
-                await bot.send_message(key, "Комната " + rooms[key] + " успешно завершена")
-                roomId = getRoomId()
-                rooms[key] = roomId
-                sm = await bot.send_message(key, "Ваша комната - " + getRoomLink(roomId))
-                await bot.pin_chat_message(key, sm.message_id)
+        try:
+            await asyncio.sleep(5)
+            for key in rooms:
+                if isRoomComplete(rooms[key]):
+                    await bot.send_message(key, "Комната " + rooms[key] + " успешно завершена")
+                    roomId = getRoomId()
+                    rooms[key] = roomId
+                    sm = await bot.send_message(key, "Ваша комната - " + getRoomLink(roomId))
+                    await bot.pin_chat_message(key, sm.message_id)
+        except:
+            pass
 
 
 import asyncio        
